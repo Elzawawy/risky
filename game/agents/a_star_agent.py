873b54a1a0fbcb49dfbc7f264f1edcae894f24a1
Elzawawy <amr22.elzawawy@gmail.com>
2019-11-26 22:21:07 +0200

    Added all needed work and comments in Agents.

--- game/agents/a_star_agent.py
+++ game/agents/a_star_agent.py
@@ -1,10 +1,12 @@
 from game.agents.base_agent import BaseAgent
 from utils.search_algorithms import a_star_search
 from utils.common_utils import back_track_path
+from game.action_handlers.risk_visitor import RiskVisitor
 class AStarAgent(BaseAgent):
 
     def __init__(self,player_name):
         super().__init__(player_name)
+        self.visitor = RiskVisitor(player_name)
 
     def place_initial_troops(self, initial_state, num_troops):
         """ Place Initial Troops on board. Executed once at begining of game.
@@ -18,15 +20,17 @@ class AStarAgent(BaseAgent):
         # TODO: Should call here random_initial_reinforcement()
         raise(NotImplementedError)
 
-    def take_turn(self, current_state, heuristic):
+    def take_turn(self, current_state, heuristic, goal_test):
         """ Take Turn in game. Executed each turn on agents.
 
             Arguments:\\
                 * current_state: The current Map State of the game.\\
                 * heuristic: The Game heuristic employed in our RISK.\\
+                * goal_test: The goal test to terminate employed in our RISK.\\
             Returns:\\
                 * result_state: The resulting Map State of the game that should be played.
         """
+        # Start working as if root of tree from current_state as no need for whole game tree.
         current_state.parent = None
-        goal_state = a_star_search(current_state, heuristic, current_state.cost)
+        goal_state = a_star_search(current_state, goal_test, heuristic, self.visitor)
         return back_track_path(goal_state)[0]
--- game/agents/minimax_agent.py
+++ game/agents/minimax_agent.py
@@ -1,9 +1,13 @@
 from game.agents.base_agent import BaseAgent
-
+from utils.search_algorithms import real_time_minimax_alpha_beta_pruning
+from game.action_handlers.risk_visitor import RiskVisitor
+from utils.common_utils import back_track_path
 
 class MinimaxAgent(BaseAgent):
-    def __init__(self, player_name):
+    def __init__(self, player_name, opponent_name):
         super().__init__(player_name)
+        self.opponent_name = opponent_name
+        self.visitor = RiskVisitor(player_name)
 
     def place_initial_troops(self, initial_state, num_troops):
         """ Place Initial Troops on board. Executed once at begining of game.
@@ -17,12 +21,18 @@ class MinimaxAgent(BaseAgent):
         # TODO: Should call here random_initial_reinforcement()
         raise(NotImplementedError)
 
-    def take_turn(self, current_state):
+    def take_turn(self, current_state, heuristic, utility, cutoff_test):
         """ Take Turn in game. Executed each turn on agents.
 
             Arguments:\\
                 * current_state: The current Map State of the game.\\
+                * heuristic: The Game heuristic employed in our RISK.\\
+                * utility: The utility function for minimax algorithm employed in our RISK.\\
+                * cutoff_test: the cutoff for real time minimax algorithm employed in our RISK.\\
             Returns:\\
-                * result_state: The resulting Map State of the game.
+                * result_state: The resulting Map State of the game that should be played.
         """
-        raise(NotImplementedError)
+        current_state.parent = None
+        goal_state = real_time_minimax_alpha_beta_pruning(current_state, self.player_name,
+            self.opponent_name, heuristic, utility, cutoff_test,  self.visitor)
+        return back_track_path(goal_state)[0]
