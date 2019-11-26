8de0154d5ff9f2f9507be49aeb9ad1b1e32b569b
MostafaYousry <mostafayousry8@gmail.com>
2019-11-26 16:17:34 +0200

    add risk game class

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from utils.search_algorithms import greedy_best_first_search
+from utils.search_algorithms import real_time_a_star_search
 from game.components import *
 from game.action_handlers.risk_visitor import RiskVisitor
+from game.game import RiskGame
 
 if __name__ == "__main__":
+
+    risk_game = RiskGame()
+
     territory1 = Territory("ALexandria", "Swidan", 5)
     territory2 = Territory("Cairo", "Swidan", 4)
     territory3 = Territory("Luxor", "Mostafa", 3)
@@ -12,4 +16,4 @@ if __name__ == "__main__":
                                  territory3:[territory1]}
 
     initial_state = RiskGameState(territory_neighbours_dict, "Swidan")
-    greedy_best_first_search(initial_state, lambda x: 1, RiskVisitor())
+    real_time_a_star_search(initial_state, risk_game.is_goal, risk_game.heuristic, RiskVisitor())
