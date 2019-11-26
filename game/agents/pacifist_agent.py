b63b1a04338af4164f7633a2775a6998f0c6468f
MostafaYousry <mostafayousry8@gmail.com>
2019-11-26 22:22:23 +0200

    implement place initial armies

--- game/agents/pacifist_agent.py
+++ game/agents/pacifist_agent.py
@@ -9,15 +9,15 @@ class PacifistAgent(BaseAgent):
     def __init__(self, player_name):
         super().__init__(player_name)
 
-    def place_initial_troops(self, initial_state, num_troops):
-        """ Place Initial Troops on board. Executed once at begining of game.
+    def place_initial_armies(self, initial_state):
+        """ Place Initial Armies on board. Executed once at begining of game.
 
             Arguments:\\
                 * current_state: The current Map State of the game.\\
-                * num_troops: The number of troops to reinforce with.\\
             Returns:\\
                 * result_state: The resulting Map State of the game.
         """
+        ARMIES_NUMBER = 1
         seed(1)
         # Get all owned territories
         owned_territories = initial_state.get_owned_territories(
@@ -25,7 +25,7 @@ class PacifistAgent(BaseAgent):
 
         # Add one army to a random territory
         owned_territories[randint(
-            0, len(owned_territories) - 1)].number_of_armies += 1
+            0, len(owned_territories) - 1)].number_of_armies += ARMIES_NUMBER
 
     def take_turn(self, current_state):
         """ Take Turn in game. Executed each turn on agents.
